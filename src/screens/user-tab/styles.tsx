cda90cf17748f7390a103ca68bbc5bcd26bf1a77
bilalqureshi <furiousali1234@gmail.com>
2023-08-23 16:28:13 +0500

    changes

--- src/screens/user-tab/styles.tsx
+++ src/screens/user-tab/styles.tsx
@@ -93,6 +93,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     alignSelf: 'center',
+    backgroundColor: colors.white,
   },
 });
 export default styles;
